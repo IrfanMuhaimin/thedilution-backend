7f00449bffec38eb361610c5b0dbb6efdcb32da4
Irfan Muhaimin <irfanmuhaimin09@gmail.com>
2025-11-14 11:37:04 +0800

    PORT standardization

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ require("./routes/hardwareLog.routes")(app);
 require("./routes/notification.routes")(app);
 
 // --- Server Listening ---
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
